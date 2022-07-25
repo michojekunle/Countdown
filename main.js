48edcc71de6b6fa1ed4af549ebaed13b2b5410e8
AMD <michojekunle1@gmail.com>
2022-07-25 17:34:40 +0100

    started js file

---
+++ main.js
@@ -0,0 +1,9 @@
+const dayEl = document.querySelector('.dayNM');
+const hourEl = document.querySelector('.hourNM');
+const minutesEl = document.querySelector('.minutesNM');
+const secondsEl = document.querySelector('.secondsNM');
+
+function updateTime() {
+    const date = new Date('2022-');
+    
+}
